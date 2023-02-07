5e538da1408dbc6c4e936f04fc3c86c9e3a68536
AnneHelin <77729495+AnneHelin@users.noreply.github.com>
2023-02-07 14:15:32 +0200

    Projektin aloitus

---
+++ kalenteri/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Question(models.Model):
+    Question_text = models.CharField(max_length=100)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=100)
+    votes = models.IntegerField(default=0)     
