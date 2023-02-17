7284eca5e220145686c47d9e139a797aa15d48d1
AnneHelin <77729495+AnneHelin@users.noreply.github.com>
2023-02-17 15:18:31 +0200

    Tilanne 170223

--- kalenteri/views.py
+++ kalenteri/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.http import Http404
 from django.shortcuts import render
+
 # Create your views here.
 
 def index(request):
     return HttpResponse("Tervetuloa omaan kalenteriisi")
 
+   
