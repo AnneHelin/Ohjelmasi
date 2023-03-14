a3a183c0de4e5fdf6d903b9f5cc391d2bd3d0e67
AnneHelin <77729495+AnneHelin@users.noreply.github.com>
2023-03-14 14:49:52 +0200

    Hommat140323

--- kalenteri/urls.py
+++ kalenteri/urls.py
@@ -5,6 +5,12 @@ from . import views
 urlpatterns = [
     # ex: /kalenteri
     path('', views.Index, name='index'),      
+    # ex: /kelenteri/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /kalenteri/5/results/
+    path('<int:question_id>/results', views.results, name='results'),
+    # ex: /kalenteri/5/votel/
+    path('<int:question_id/vote/', views.vote, name='vote'),
        
 ]   
    
--- kalenteri/views.py
+++ kalenteri/views.py
@@ -1,7 +1,24 @@
 from django.http import HttpResponse
-
-
-  
+from django.template import loader
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Kalenterisi
 def Index(request):
     return HttpResponse("Hello! Welcomen, to your calendar")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(reguest, question_id):
+    reponse = "You're looking at the results of question %s."
+    return HttpResponse(reponse % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def index(request):
+    latest_question_list = Kalenterisi.objects.order_by('-pub_date')[:5]
+    output = ','.join([q.kalenteri_text for q in latest_question_list])
+    return HttpResponse(output)
+
 
