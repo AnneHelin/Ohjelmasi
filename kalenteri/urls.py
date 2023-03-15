d793289fe9826ffc1874321a7fd449c775d9cf7b
AnneHelin <77729495+AnneHelin@users.noreply.github.com>
2023-03-15 14:36:08 +0200

    Toimintojen muutos150323

--- kalenteri/urls.py
+++ kalenteri/urls.py
@@ -1,16 +1,19 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
 from django.urls import path
-
-from . import views
+from kalenteri import views
 
 urlpatterns = [
-    # ex: /kalenteri
-    path('', views.Index, name='index'),      
-    # ex: /kelenteri/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /kalenteri/5/results/
-    path('<int:question_id>/results', views.results, name='results'),
-    # ex: /kalenteri/5/votel/
-    path('<int:question_id/vote/', views.vote, name='vote'),
-       
-]   
-   
+    path('admin/', admin.site.urls),
+
+    path('', views.EtusivuView.as_view(), name='etusivu'),
+    path('kirjautuminen/', views.kirjautuminen, name='kirjautuminen'),
+    path('uloskirjautuminen/', views.uloskirjautuminen, name='uloskirjautuminen'),
+
+    path('muokkaa-kayttajaa/', views.MuokkaaKayttajaaView.as_view(), name='muokkaa-kayttajaa'),
+    path('vaihda-salasanaa//', views.VaihdaSalasanaaView.as_view(), name='vaihda-salasanaa'),
+
+    path('hallinta/', views.HallistaView.as_view(), name='hallinta'), 
+    path('lisaa-tapahtuma/', views.LisaaTapahtumaView.as_view(), name='lisaaminen')
+    ]
