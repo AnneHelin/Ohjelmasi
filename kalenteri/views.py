269e032173f884ebb43de4f1ce8e9166a6111afe
AnneHelin <77729495+AnneHelin@users.noreply.github.com>
2023-03-17 10:25:23 +0200

    poisto

--- kalenteri/views.py
+++ kalenteri/views.py
@@ -1,9 +1,3 @@
-from django.http import HttpResponse
-from django.template import loader
-from .models import Kalenterisi
-
-def Index(request):
-    return HttpResponse("Hello! Welcomen, to your calendar")
 
 
 
