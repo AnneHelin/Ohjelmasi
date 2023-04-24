7165bfd2234b5f37723b659c90d8c205e0330152
AnneHelin <77729495+AnneHelin@users.noreply.github.com>
2023-04-24 14:56:59 +0300

    240423Tapa

--- kalenteri/urls.py
+++ kalenteri/urls.py
@@ -4,10 +4,15 @@ from django.http import HttpResponse, JsonResponse
 
 
 urlpatterns = [
-    path('', views.Tapahtuma, name='index'),
-    path('tapahtuma', views.Tapahtuma, name='tapahtuma'),
-
-]
+        # Path Converters
+        # int: numbers
+        # str: tring
+        # path: whole urls/
+        # slug: hyphen-and_underscores_stuff
+        # UUID: universally unique identifer
+        #path(', views.tervetuloa, name="tervetuloa"),
+        path('<int:year>/<str:month>/', views.kalenteri, name="kalenteri"),
+        ]
 
 
 
