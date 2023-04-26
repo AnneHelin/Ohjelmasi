15cc48794ee25fa78e7fc77d323a0ba9c08900f4
AnneHelin <77729495+AnneHelin@users.noreply.github.com>
2023-04-26 14:52:09 +0300

    Lisätty260423

--- kalenteri/views.py
+++ kalenteri/views.py
@@ -1,15 +1,28 @@
-from django.http import( 
-    HttpResponse, 
-    HttpResponseBadRequest, 
-    HttpResponseNotFound
-)    
 from django.shortcuts import render
+import kalenteri
+from kalenteri import HTMLKalenteri
+from datetime import datetime
 
-from .models import Kalenteri
 
-def kalenteri(request):
-    "Tervetuloa kalenteriin!"
-    return render(request,'index.html')
+def kalenteri(request, year, month):
+    name = "Tervetuloa kalenteriisi!"
+    month = month.capitalize()
+    # Convert month from name to number
+    month_number = list(kalenteri.month_name).index(month)
+    month_number = int(month_number)
 
-def kalenteri(request year, month):
-    
+    # create  a calendar
+    cal = HTMLKalenteri().formatmonth(
+        year,
+        month_number)
+    # Get current year
+    now = datetime.now() 
+    current_year = now.year
+    return render(request,
+                    'tervetuloa.html', {
+                    "name" : name,
+                    "year" : year,
+                    "month" : month,
+                    "month_number" : month_number,
+                    "current_year": current_year,
+                  })
