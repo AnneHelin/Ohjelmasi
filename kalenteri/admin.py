b495895b5d3fd25b7b2b71957be4d3481f3648fc
AnneHelin <77729495+AnneHelin@users.noreply.github.com>
2023-05-09 15:03:46 +0300

    090523tehdytmuutokset

--- kalenteri/admin.py
+++ kalenteri/admin.py
@@ -1,25 +1,10 @@
 from django.contrib import admin
-from kalenteri import models
+from django.urls import path, include
 
-@admin.register(models.Tapahtuma)
-class TapahtumaAdmin(admin.ModelAdmin):
-    model = models.Tapahtuma
-    list_display = [
-        "id",
-        "title",
-        "user",
-        "is_active",
-        "is_deleted",
-        "created_at",
-        "updated_at"
-    
-    ]
-    list_filter = ["is_active", "is_deleted"]
-    search_fields = ["title"]
+urlpatterns = [
+    path('admin/', admin.site.urls),
 
+    # Enter the app
 
-@admin.register(models.Tapahtuma)
-class Tapahtumakalenteri(admin.ModelAdmin):
-    model = models.Tapahtuma
-    list_display = ["id", "tapahtuma", "user", "created_at", "uppdated_at"]
-    list_filter = ["tapahtuma"]
+    # path('', include("kalenteri.app"))
+]
