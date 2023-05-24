5cb07e3bc04bb4428c5fe5e9b32d6ede8f303973
AnneHelin <77729495+AnneHelin@users.noreply.github.com>
2023-05-24 14:36:16 +0300

    Lisätty 240523

--- kalenteri/urls.py
+++ kalenteri/urls.py
@@ -13,7 +13,10 @@ urlpatterns = [
         # UUID: universally unique idemtifier
         # path('', views.index, name='index'),
         path("" , views.index, name="index"),
-        
+        path("<int:year_id/", views.etusivu, name='etusivu'),
+        path("<int:month_id/", views.etusivu, name='etusivu'),
+        path("<int:day_id/", views.day, name='day'),
+        path('<request/', views.kirjaa_ohjelmasi, name='kirjaa_ohjelmasi'),
 
         # path('', views.kalenteri, name="kalenteri"),
        # path('<int:year> / <str:month> / <str:day>', views.kalenteri, name='kalenteri'),
